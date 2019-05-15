be08e0a974a9ce66d75a0dc7ef964d8957f2510e
shedprog <flynik75@gmial.com>
2019-05-15 19:28:58 +0300

    fix TSting path to files

--- src/LepE_calibr.h
+++ src/LepE_calibr.h
@@ -2,14 +2,16 @@
 #ifndef LepE_calibr_h
 #define LepE_calibr_h
 
+#include <stdlib.h>
+#include <string>
 #include "TFile.h"
 #include "TH1D.h"
 #include "TString.h"
 
-const TString 0506e_mc   = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_Ariadne_Low_Q2_NC_DIS_0506e.list.root";
-const TString 0506e_data = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_0506e.list.root";
-const TString 0607p_mc   = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_Ariadne_Low_Q2_NC_DIS_0607p.list.root";
-const TString 0607p_data = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_0607p.list.root";
+TString path0506e_mc   = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_Ariadne_Low_Q2_NC_DIS_0506e.list.root";
+TString path0506e_data = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_0506e.list.root";
+TString path0607p_mc   = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_Ariadne_Low_Q2_NC_DIS_0607p.list.root";
+TString path0607p_data = "/nfs/dust/zeus/group/mykytaua/Jets_nfs/2019.05.15/control_plots_clean/root/output_Sample_0607p.list.root";
 
 class Calibrator {
 private:
@@ -18,14 +20,17 @@ private:
 public:
 	Calibrator(TString period)
 	{
+		TFile* fdata;
+		TFile* fmc;
+
 		if(period == "0405e" || period == "06e" || period == "0506e" || period == "05e"){
-			TFile* fdata = new TFile(0506e_data);  
-        		TFile* fmc = new TFile(0506e_mc);
+			fdata = new TFile(path0506e_data);  
+        		fmc = new TFile(path0506e_mc);
 		}
 
 		else if(period == "0607p" || period == "06p" || period == "07p"){
-			TFile* fdata = new TFile(0607p_data);  
-        		TFile* fmc = new TFile(0607p_mc);
+			fdata = new TFile(path0607p_data);  
+        		fmc = new TFile(path0607p_mc);
 		}
 		else{
 			std::cout<<"No such period!"<<"\n";
